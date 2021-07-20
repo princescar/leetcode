d9133c87b6a4dc77f1287e113ab7fa9217801a21
Halbert Lei <halbert.lei@outlook.com>
2021-07-20 16:39:36 +0800

    cleaner solution for 19.

--- 19.js
+++ 19.js
@@ -11,36 +11,17 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-  const l = reverse(head);
-  const l2 = remove(l, n);
-  return reverse(l2);
-
-  function reverse(list) {
-    let node = list;
-    let previous = null;
-    while(node) {
-      const current = node;
-      node = node.next;
-      current.next = previous;
-      previous = current;
-    }
-    return previous;
-  }
-
-  function remove(list, n) {
-    let counter = 1;
-    let node = list;
-    let previous = null;
-    while (counter !== n) {
-      previous = node;
-      node = node.next;
-      counter++;
-    }
-    if (previous) {
-      previous.next = node.next;
-      return list;
+  const dummy = new ListNode(0, head);
+  let first = dummy, second = dummy;
+  let gap = 0;
+  while (first) {
+    first = first.next;
+    if (gap === n + 1) {
+      second = second.next;
     } else {
-      return node.next;
+      gap++;
     }
   }
+  second.next = second.next.next;
+  return dummy.next;
 };
