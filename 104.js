44c9d2c4e51d53539b48b79a1068c60e59a55e06
Halbert Lei <halbert.lei@outlook.com>
2021-07-22 22:57:04 +0800

    recursive solution for 104.

---
+++ 104.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  const c = n => n ? Math.max(c(n.left), c(n.right)) + 1 : 0;
+  return c(root);
+};
