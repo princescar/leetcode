6ae35b8cef0f599f9691c7e5b56c6498a2fe4867
Halbert Lei <halbert.lei@outlook.com>
2021-07-28 09:56:14 +0800

    loop solution for 202.

--- 202.js
+++ 202.js
@@ -3,15 +3,17 @@
  * @return {boolean}
  */
 var isHappy = function(n) {
-  return h(n);
-  function h(x, previous) {
-    if (x === 1) return true;
-    let y = x, next = 0;
-    while (y > 0) {
-      next += Math.pow(y % 10, 2);
+  const chain = [n]
+  let x = n;
+  while(x !== 1) {
+    let y = x, r = 0;
+    while(y > 0) {
+      r += Math.pow(y % 10, 2);
       y = Math.floor(y / 10);
     }
-    if (next === previous) return false;
-    return h(next, x);
+    if (chain.includes(r)) return false;
+    chain.push(x);
+    x = r;
   }
+  return true;
 };
