b0fe437e6705162f2ff4e0b6416cc1d563c4c916
Halbert Lei <halbert.lei@outlook.com>
2021-07-29 21:30:16 +0800

    fix solution for 387.

--- 387.js
+++ 387.js
@@ -6,11 +6,12 @@ var firstUniqChar = function(s) {
   const first = new Map();
   for(let i = 0; i < s.length; i++) {
     const ch = s.charCodeAt(i);
-    if (first.get(ch) == null) {
+    if (!first.has(ch)) {
       first.set(ch, i);
     } else {
       first.set(ch, -1);
     }
   }
-  return Math.min(...first.values()) || -1;
+  const r = [...first.values()].filter(x => x >= 0);
+  return r.length > 0 ? Math.min(...r) : -1;
 };
