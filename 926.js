8049515ae3202161120d56173fccf88251c2fe71
Halbert Lei <halbert.lei@outlook.com>
2021-08-11 14:56:38 +0800

    loop solution for 926.

--- 926.js
+++ 926.js
@@ -3,20 +3,22 @@
  * @return {number}
  */
 var minFlipsMonoIncr = function(s) {
-  const count0 = [];
-  count0[s.length] = 0;
-  for (let i = s.length - 1; i >= 0; i--) {
-    count0[i] = count0[i + 1];
-    if (s[i] === '0') count0[i]++;
+  const count0 = [0];
+  for (let i = 0, c0 = 0; i < s.length; i++) {
+    if (s[i] === '0') c0++;
+    count0[i + 1] = c0;
   }
-  return flip(0);
 
-  function flip(start) {
-    if (start >= s.length) return 0;
-    let i = start;
-    while (s[i] === '0') i++;
-    let j = i;
-    while (s[j] === '1') j++;
-    return Math.min((j - i) + flip(j), count0[j]);
+  let min = Infinity;
+  for (let i = 0; i <= s.length; i++) {
+    // Should have (i) 0s in the front.
+    const c0front = count0[i]; // Alrady have (count0[i]) 0s in the front.
+    const flip0 = i - c0front; // Need to flip (i - count0[i]) 1s to 0s in the front.
+
+    // Should have (s.length - i) 1s after 0s.
+    const flip1 = count0[s.length] - c0front; // Have (count0[s.length] - c0front) 0s in the back, they all need to be flip to 1.
+    const flips = flip0 + flip1;
+    if (flips < min) min = flips;
   }
+  return min;
 };
