bce81d5cb59c467f0ffbdc8051cb8ae73067d839
Halbert Lei <halbert.lei@outlook.com>
2021-08-14 09:56:47 +0800

    space optimized solution for 73.

--- 73.js
+++ 73.js
@@ -3,20 +3,44 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
-  const rows = new Set(), cols = new Set();
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[0].length; j++) {
+  const m = matrix.length, n = matrix[0].length;
+  let col0 = false, row0 = false;
+  for (let i = 0; i < m; i++) {
+    if (matrix[i][0] === 0) {
+      col0 = true;
+    }
+  }
+  for (let j = 0; j < n; j++) {
+    if (matrix[0][j] === 0) {
+      row0 = true;
+    }
+  }
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
       if (matrix[i][j] === 0) {
-        rows.add(i);
-        cols.add(j);
+        matrix[0][j] = 0;
+        matrix[i][0] = 0;
       }
     }
   }
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[0].length; j++) {
-      if (rows.has(i) || cols.has(j)) {
+  for (let i = 1; i < m; i++) {
+    if (matrix[i][0] === 0) {
+      for (let j = 1; j < n; j++) {
         matrix[i][j] = 0;
       }
     }
+  }
+  for (let j = 1; j < n; j++) {
+    if (matrix[0][j] === 0) {
+      for (let i = 1; i < m; i++) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+  if (col0) {
+    for (let i = 0; i < m; i++) matrix[i][0] = 0;
+  }
+  if (row0) {
+    for (let j = 0; j < n; j++) matrix[0][j] = 0;
   }
 };
