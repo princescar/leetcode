0e681439d336ac1dfc3d86d009a04de800f220f2
Halbert Lei <halbert.lei@outlook.com>
2021-08-16 17:06:15 +0800

    simpler solution for 57.

--- 57.js
+++ 57.js
@@ -4,29 +4,24 @@
  * @return {number[][]}
  */
 var insert = function(intervals, newInterval) {
-  let l = -1, r = -1;
+  const r = [];
+  let inserted = false;
   for (let i = 0; i < intervals.length; i++) {
-    if (intervals[i][0] > newInterval[0]) {
-      l = i - 1;
-      r = i;
-      break;
-    }
+    if (intervals[i][1] < newInterval[0]) {
+      r.push(intervals[i]);
+    } else if (intervals[i][0] > newInterval[1]) {
+      if (!inserted) {
+        r.push(newInterval);
+        inserted = true;
+      }
+      r.push(intervals[i]);
+    } else {
+      newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+      newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+    };
   }
-  if (r < 0) l = intervals.length - 1;
-  while (l >= 0 && intervals[l][1] >= newInterval[0]) {
-    newInterval[0] = Math.min(intervals[l][0], newInterval[0]);
-    newInterval[1] = Math.max(intervals[l][1], newInterval[1]);
-    intervals.splice(l, 1);
-    l--;
-    r--;
+  if (!inserted) {
+    r.push(newInterval);
   }
-  if (r >= 0) {
-    while (r < intervals.length && intervals[r][0] <= newInterval[1]) {
-      newInterval[0] = Math.min(intervals[r][0], newInterval[0]);
-      newInterval[1] = Math.max(intervals[r][1], newInterval[1]);
-      intervals.splice(r, 1);
-    }
-  }
-  intervals.splice(l + 1, 0, newInterval);
-  return intervals;
+  return r;
 };
