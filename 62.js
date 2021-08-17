5f96f1917c8f1c314eaa3ec7971ff073fbabd94e
Halbert Lei <halbert.lei@outlook.com>
2021-08-17 08:41:22 +0800

    solution for 62.

---
+++ 62.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+  const dp = [];
+  return path(0, 0);
+
+  function path(i, j) {
+    if (i === m - 1 && j === n - 1) return 1;
+    if (!dp[i]) dp[i] = [];
+    if (dp[i][j]) return dp[i][j];
+    let r = 0;
+    if (i < m - 1) r += path(i + 1, j);
+    if (j < n - 1) r += path(i, j + 1);
+    dp[i][j] = r;
+    return r;
+  }
+};
