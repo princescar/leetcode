fd71a00618b2044efc6ba61906c5d4cb47cd35c4
Halbert Lei <halbert.lei@outlook.com>
2021-08-23 21:14:12 +0800

    solution for 1646.

---
+++ 1646.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var getMaximumGenerated = function(n) {
+  const map = [0, 1];
+  let max = 0;
+  for (let i = 1; i <= n; i += 2) {
+    const a = gen(i);
+    if (a > max) max = a;
+  }
+  return max;
+
+  function gen(x) {
+    if (map[x] != null) return map[x];
+    if (x % 2) {
+      const i = Math.floor(x / 2);
+      return gen(i) + gen(i + 1);
+    } else {
+      return gen(x >> 1);
+    }
+  }
+};
+console.time();
+console.log(getMaximumGenerated(100));
+console.timeEnd();
