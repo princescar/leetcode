568876f6b2a8fbaa8b1f1f569554a4f7c99f115c
Halbert Lei <halbert.lei@outlook.com>
2021-08-27 12:48:28 +0800

    loop solution for 331.

--- 331.js
+++ 331.js
@@ -4,15 +4,12 @@
  */
 var isValidSerialization = function(preorder) {
   const arr = preorder.split(',');
-  return arr.length % 2 &&
-    (arr.length === 1 && arr[0] === '#' ||
-      arr[arr.length - 1] === '#' &&
-      arr[arr.length - 2] === '#') &&
-    traverse(0) === arr.length;
-
-  function traverse(start) {
-    if (start >= arr.length) return start + 1;
-    if (arr[start] === '#') return start + 1;
-    else return traverse(traverse(start + 1));
+  if (arr.length % 2 === 0) return false;
+  let slots = 1;
+  for (const x of arr) {
+    if (slots === 0) return false;
+    if (x === '#') slots--;
+    else slots++;
   }
+  return slots === 0;
 };
