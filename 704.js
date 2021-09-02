c2ce8b7f5a682bbf566425018ad383294b3c9637
Halbert Lei <halbert.lei@outlook.com>
2021-09-02 19:02:21 +0800

    solution for 704.

---
+++ 704.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+  let l = 0, r = nums.length - 1;
+  while (l <= r) {
+    const mid = Math.floor((l + r) / 2);
+    if (nums[mid] === target) return mid;
+    else if (nums[mid] > target) r = mid - 1;
+    else l = mid + 1;
+  }
+  return -1;
+};
