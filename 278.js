45412ea2e6a6299edfb260b325e773dff1eac04c
Halbert Lei <halbert.lei@outlook.com>
2021-09-03 09:57:57 +0800

    solution for 278.

---
+++ 278.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function(n) {
+    let l = 1, r = n;
+    while (l < r) {
+      const mid = Math.floor((l + r) / 2);
+      const bad = isBadVersion(mid);
+      if (bad) r = mid;
+      else l = mid + 1;
+    }
+    return l;
+  };
+};
