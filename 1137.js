656638af4086260f98673c306bbf88a98e1b736d
Halbert Lei <halbert.lei@outlook.com>
2021-09-03 11:04:40 +0800

    O(1) space solution for 1137.

--- 1137.js
+++ 1137.js
@@ -3,10 +3,14 @@
  * @return {number}
  */
 var tribonacci = function(n) {
-  const cache = [0, 1, 1];
-  if (n < 3) return cache[n];
+  if (n === 0) return 0;
+  if (n < 3) return 1;
+  let c1 = 0, c2 = 1, c3 = 1;
   for (let i = 3; i <= n; i++) {
-    cache[i] = cache[i - 1] + cache[i - 2] + cache[i - 3];
+    const temp = c1;
+    c1 = c2;
+    c2 = c3;
+    c3 = temp + c1 + c2;
   }
-  return cache[n];
+  return c3;
 };
