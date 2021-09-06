44d203ee484bebf4c798c360a70c57037b212310
Halbert Lei <halbert.lei@outlook.com>
2021-09-06 18:04:14 +0800

    loop solution for 746.

--- 746.js
+++ 746.js
@@ -4,13 +4,9 @@
  */
 var minCostClimbingStairs = function(cost) {
   const n = cost.length;
-  const cache = [];
-  return Math.min(climb(0), climb(1));
-
-  function climb(i) {
-    if (i >= n) return 0;
-    if (cache[i] == null)
-      cache[i] = cost[i] + Math.min(climb(i + 1), climb(i + 2));
-    return cache[i];
+  const cache = [cost[0], cost[1]];
+  for (let i = 2; i < n; i++) {
+    cache[i] = cost[i] + Math.min(cache[i - 1], cache[i - 2]);
   }
+  return Math.min(cache[n - 1], cache[n - 2]);
 };
