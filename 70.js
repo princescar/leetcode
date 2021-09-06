b02c259a642e563b4378e2f1eeee09b4e767dca8
Halbert Lei <halbert.lei@outlook.com>
2021-09-06 17:29:15 +0800

    loop solution for 70.

--- 70.js
+++ 70.js
@@ -1,16 +1,12 @@
-const cache = [, 1, 2];
-
 /**
  * @param {number} n
  * @return {number}
  */
 var climbStairs = function(n) {
-  return c(n);
-
-  function c(x) {
-    if (cache[x]) return cache[x];
-    const r = c(x - 1) + c(x - 2);
-    cache[x] = r;
-    return r;
+  const cache = [,1,2];
+  if (n < 3) return cache[n];
+  for (let i = 3; i <= n; i++) {
+    cache[i] = cache[i - 1] + cache[i - 2];
   }
+  return cache[n];
 };
