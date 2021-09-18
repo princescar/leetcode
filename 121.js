14b5b19dece65d990e996e03686f191f5ed29b2a
Halbert Lei <halbert.lei@outlook.com>
2021-09-18 13:19:03 +0800

    simplified solution for 121.

--- 121.js
+++ 121.js
@@ -2,17 +2,11 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
-  let low = prices[0];
-  let max = 0;
+ var maxProfit = function(prices) {
+  let max = 0, low = prices[0];
   for (let i = 1; i < prices.length; i++) {
-    const x = prices[i];
-    if (x < low) {
-      low = x;
-    } else {
-      const p = x - low;
-      if (p > max) max = p;
-    }
+    max = Math.max(max, prices[i] - low);
+    low = Math.min(low, prices[i]);
   }
   return max;
 };
