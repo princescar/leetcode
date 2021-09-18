ce837fb9a162343c890b7820d7bad8334afb65d7
Halbert Lei <halbert.lei@outlook.com>
2021-09-18 15:19:45 +0800

    loop solution for 740.

--- 740.js
+++ 740.js
@@ -9,13 +9,12 @@ var deleteAndEarn = function(nums) {
     map.set(x, (map.get(x) || 0) + 1);
     max = Math.max(max, x);
   }
-  const dp = [];
-  return del(1);
-
-  function del(x) {
-    if (x > max) return 0;
-    if (dp[x] == null)
-      dp[x] = Math.max((map.get(x) || 0) * x + del(x + 2), del(x + 1));
-    return dp[x];
+  const cache = [0];
+  for (let i = 1; i <= max; i++) {
+    cache[i] = Math.max(
+      (map.get(i) || 0) * i + (cache[i - 2] || 0),
+      cache[i - 1],
+    );
   }
+  return cache[max];
 };
