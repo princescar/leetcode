00441b3cca37cb1a20d0e24a60ce552e571a833d
Halbert Lei <halbert.lei@outlook.com>
2021-09-18 12:19:57 +0800

    backward solution for 88.

--- 88.js
+++ 88.js
@@ -5,25 +5,11 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function(nums1, m, nums2, n) {
-  if (n === 0) return;
-
-  for (let i = 0; i < m; i++) {
-    const a = nums1[i];
-    if (a > nums2[0]) {
-      nums1[i] = nums2[0];
-      nums2[0] = a;
-      for (let j = 1; j < n; j++) {
-        if (nums2[j - 1] > nums2[j]) {
-          const b = nums2[j];
-          nums2[j] = nums2[j - 1];
-          nums2[j - 1] = b;
-        }
-      }
-    }
-  }
-
-  for (let i = 0; i < n; i++) {
-    nums1[m + i] = nums2[i];
+ var merge = function(nums1, m, nums2, n) {
+  let i = m - 1, j = n - 1, k = m + n - 1;
+  while (j >= 0) {
+    if (i < 0) nums1[k--] = nums2[j--];
+    else if (nums1[i] <= nums2[j]) nums1[k--] = nums2[j--];
+    else nums1[k--] = nums1[i--];
   }
 };
