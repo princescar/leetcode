46eae7d8b5bd42f3a09858f7db6b9107e7c42644
Halbert Lei <halbert.lei@outlook.com>
2021-09-24 16:52:15 +0800

    another solution for 387.

--- 387.js
+++ 387.js
@@ -2,16 +2,15 @@
  * @param {string} s
  * @return {number}
  */
-var firstUniqChar = function(s) {
-  const first = new Map();
-  for(let i = 0; i < s.length; i++) {
-    const ch = s.charCodeAt(i);
-    if (!first.has(ch)) {
-      first.set(ch, i);
-    } else {
-      first.set(ch, -1);
-    }
+ var firstUniqChar = function(s) {
+  const map = new Map();
+  for (let i = 0; i < s.length; i++) {
+    if (map.has(s[i])) map.set(s[i], -1);
+    else map.set(s[i], i);
   }
-  const r = [...first.values()].filter(x => x >= 0);
-  return r.length > 0 ? Math.min(...r) : -1;
+  let min = Infinity;
+  for (const i of map.values()) {
+    if (i >= 0 && i < min) min = i;
+  }
+  return min === Infinity ? -1 : min;
 };
