97a4dcd18fb773742a38e2849353370584a4532c
Halbert Lei <halbert.lei@outlook.com>
2021-10-05 11:27:05 +0800

    voting solution for 169.

--- 169.js
+++ 169.js
@@ -3,15 +3,16 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-  const count = new Map();
-  for (let i = 0; i < nums.length; i++) {
-    const x = nums[i];
-    let c = count.get(x);
-
-    if (c) count.set(x, c + 1);
-    else count.set(x, 1);
-
-    c = count.get(x);
-    if (c > nums.length / 2) return x;
+  let candidate = null, count = 0;
+  for (const x of nums) {
+    if (count === 0) {
+      candidate = x;
+      count = 1;
+    } else if (candidate === x) {
+      count++;
+    } else {
+      count--;
+    }
   }
+  return candidate;
 };
