6c9b5a9090192ef83fcf49b0d570d71653888ece
Halbert Lei <halbert.lei@outlook.com>
2021-10-19 15:27:17 +0800

    bi-weekly contest 63.

---
+++ 2037.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} seats
+ * @param {number[]} students
+ * @return {number}
+ */
+ var minMovesToSeat = function(seats, students) {
+  seats.sort((a, b) => a - b);
+  students.sort((a, b) => a - b);
+  let ans = 0;
+  for (let i = 0; i < seats.length; i++)
+    ans += Math.abs(students[i] - seats[i]);
+  return ans;
+};
