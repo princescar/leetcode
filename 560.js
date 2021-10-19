271370cce86f8d0b322325f34a28cb4a83cf5360
Halbert Lei <halbert.lei@outlook.com>
2021-10-19 21:44:05 +0800

    map optimized solution for 560.

--- 560.js
+++ 560.js
@@ -4,13 +4,23 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
-  const sums = [0];
-  for (let i = 0; i < nums.length; i++)
-    sums[i + 1] = sums[i] + nums[i];
+  const map = new Map();
+  map.set(0, [-1]);
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    if (map.has(sum)) map.get(sum).push(i);
+    else map.set(sum, [i]);
+  }
   let ans = 0;
-  for (let i = 0; i < sums.length; i++) {
-    for (let j = i + 1; j < sums.length; j++) {
-      if (sums[j] - sums[i] === k) ans++;
+  sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    if (!map.has(sum - k)) continue;
+    const arr = map.get(sum - k);
+    for (const j of arr) {
+      if (j < i) ans++;
+      else break;
     }
   }
   return ans;
