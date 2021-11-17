a8855f9b922b022cc1263462f1a818fdb7c497b3
Halbert Lei <halbert.lei@outlook.com>
2021-11-17 21:16:55 +0800

    solution for 572.

---
+++ 572.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function(root, subRoot) {
+  if (!root) return false;
+  return isSame(root, subRoot) ||
+    isSubtree(root.left, subRoot) ||
+    isSubtree(root.right, subRoot);
+
+  function isSame(root1, root2) {
+    if (!root1 && !root2) return true;
+    if (!root1 || !root2) return false;
+    return root1.val == root2.val &&
+      isSame(root1.left, root2.left) &&
+      isSame(root1.right, root2.right);
+  }
+};
