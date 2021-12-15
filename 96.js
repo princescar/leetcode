737df0a6989370580b712bee41247fb582119c84
Halbert Lei <halbert.lei@outlook.com>
2021-12-15 10:30:50 +0800

    loop solution for 96.

--- 96.js
+++ 96.js
@@ -3,15 +3,12 @@
  * @return {number}
  */
 var numTrees = function(n) {
-  const cache = [1, 1];
-  return num(n);
-
-  function num(x) {
-    if (cache[x] == null) {
-      cache[x] = 0;
-      for (let i = 0; i < x; i++)
-        cache[x] += num(i) * num(x - i - 1);
+  const cache = [1];
+  for (let i = 1; i <= n; i++) {
+    cache[i] = 0;
+    for (let j = 0; j < i; j++) {
+      cache[i] += cache[j] * cache[i - j - 1];
     }
-    return cache[x];
   }
+  return cache[n];
 };
