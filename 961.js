390ca15c113be26f25344f960fb0e0e73b4e3fa2
Halbert Lei <halbert.lei@outlook.com>
2022-05-21 13:03:05 +0800

    solution for 961.

---
+++ 961.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var repeatedNTimes = function(nums) {
+  const set = new Set();
+  for (const x of nums)
+    if (set.has(x)) return x;
+    else set.add(x);
+};
